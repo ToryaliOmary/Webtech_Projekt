21ebce4cfdadc185898e77345f66f15d6ceb96de
emiliobratta <77102958+emiliobratta@users.noreply.github.com>
2021-10-18 16:48:15 +0200

    adds hello world

--- src/main/java/com/example/servingwebcontent/WebController.java
+++ src/main/java/com/example/servingwebcontent/WebController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/greetings")
+    public String showGreetingsPage(Model model) {
+        model.addAttribute("hellogreetings", "tayfun stinkt");
+        return "greetings";
     }
 }
