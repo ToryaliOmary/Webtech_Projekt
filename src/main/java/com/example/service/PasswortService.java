047052892029f3e167f9ae8ea6fe43f1d0920eae
emiliobratta <77102958+emiliobratta@users.noreply.github.com>
2021-11-14 12:55:06 +0100

    adds REST API methods

--- src/main/java/com/example/service/PasswortService.java
+++ src/main/java/com/example/service/PasswortService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import com.example.persistence.PasswortEntity;
 import com.example.persistence.PasswortRepository;
 import com.example.servingwebcontent.api.Passwort;
+import com.example.servingwebcontent.api.PasswortManipulationRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,11 +21,49 @@ public class PasswortService {
     public List<Passwort> findAll(){
         List<PasswortEntity> passworte = passwortRepository.findAll();
         return passworte.stream()
-                .map(passwortEntity -> new Passwort(
-                        passwortEntity.getId(),
-                        passwortEntity.getPasswort(),
-                        passwortEntity.getWebsite()
-                ))
+                .map(this::transformEntity)
                 .collect(Collectors.toList());
+    }
+
+    public Passwort findById(Long id) {
+        var passwortEntity = passwortRepository.findById(id);
+        return passwortEntity.map(this::transformEntity).orElse(null);
+    }
+
+    public Passwort create(PasswortManipulationRequest request) {
+        var passwortEntity = new PasswortEntity(request.getWebsite(), request.getPasswort());
+        passwortEntity = passwortRepository.save(passwortEntity);
+        return transformEntity(passwortEntity);
+    }
+
+    public Passwort update(Long id, PasswortManipulationRequest request) {
+        var passwortEntityOptional = passwortRepository.findById(id);
+        if (passwortEntityOptional.isEmpty()) {
+            return null;
+        }
+
+        var passwortEntity = passwortEntityOptional.get();
+        passwortEntity.setWebsite(request.getWebsite());
+        passwortEntity.setPasswort(request.getPasswort());
+        passwortEntity = passwortRepository.save(passwortEntity);
+
+        return transformEntity(passwortEntity);
+    }
+
+    public boolean deleteById(Long id) {
+        if (!passwortRepository.existsById(id)) {
+            return false;
+        }
+
+        passwortRepository.deleteById(id);
+        return true;
+    }
+
+    private Passwort transformEntity(PasswortEntity passwortEntity) {
+        return new Passwort(
+                passwortEntity.getId(),
+                passwortEntity.getWebsite(),
+                passwortEntity.getPasswort()
+        );
     }
 }
--- src/main/java/com/example/servingwebcontent/api/PasswortRestController.java
+++ src/main/java/com/example/servingwebcontent/api/PasswortRestController.java
@@ -1,10 +1,13 @@
 package com.example.servingwebcontent.api;
 
 import com.example.service.PasswortService;
+import com.example.servingwebcontent.api.Passwort;
+import com.example.servingwebcontent.api.PasswortManipulationRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
@@ -16,9 +19,33 @@ public class PasswortRestController {
         this.passwortService = passwortService;
     }
 
-
     @GetMapping(path = "/api/passwort")
     public ResponseEntity<List<Passwort>> fetchPasswort(){
         return ResponseEntity.ok(passwortService.findAll());
+    }
+
+    @GetMapping(path = "/api/v1/passwort/{id}")
+    public ResponseEntity<Passwort> fetchPasswortById(@PathVariable Long id) {
+        var passwort = passwortService.findById(id);
+        return passwort != null? ResponseEntity.ok(passwort) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(path = "/api/passwort")
+    public ResponseEntity<Void> createPasswort(@RequestBody PasswortManipulationRequest request) throws URISyntaxException {
+        var passwort = passwortService.create(request);
+        URI uri = new URI("/api/v1/passwort/" + passwort.getId());
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(path = "/api/v1/passwort/{id}")
+    public ResponseEntity<Passwort> updatePasswort(@PathVariable Long id, @RequestBody PasswortManipulationRequest request) {
+        var passwort = passwortService.update(id, request);
+        return passwort != null? ResponseEntity.ok(passwort) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping(path = "/api/v1/passwort/{id}")
+    public ResponseEntity<Void> deletePasswort(@PathVariable Long id) {
+        boolean successful = passwortService.deleteById(id);
+        return successful? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 }
